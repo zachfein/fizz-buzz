ce0e404cdb98e45cd34c0de91649a67eb0a2b627
zachfein <zvf0307@gmail.com>
2022-03-23 09:06:27 -0600

    completed find-the-smallest-number

--- findTheRange.js
+++ findTheRange.js
@@ -13,3 +13,12 @@ for(let i=1; i<array.length; i++){
 
 console.log(smallest);
 
+let largest = array[0];
+
+for(let i=1; i<array.length; i++){
+    if(array[i] > largest){
+        largest = array[i];
+    }
+}
+
+console.log(largest);
