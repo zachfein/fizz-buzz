6f675f5ec160addd0787741d084e87e50a87dee1
zachfein <zvf0307@gmail.com>
2022-03-22 23:00:25 -0600

    found smallest number

--- findTheRange.js
+++ findTheRange.js
@@ -1,4 +1,15 @@
 // Starting array
-let array = [28, 43, -12, 30, 4, 0, -36]
+let array = [28, 43, -12, 30, 4, 0, -36];
 
 // Write your solution below:
+
+let smallest = array[0];
+
+for(let i=1; i<array.length; i++){
+    if(array[i] < smallest){
+        smallest = array[i];
+    }
+}
+
+console.log(smallest);
+
